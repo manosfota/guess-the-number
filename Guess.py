12e217c4b9c3b6166af7cc6a16a9a02a255d72c5
Fotakis Manos <fotamano@gmail.com>
2025-05-08 16:23:15 +0100

    Add Guess the Number project

--- Guess.py
+++ Guess.py
@@ -2,22 +2,37 @@ import random as rd
 
 
 def guess():
+  secret=rd.randint(1,10)
   tries=0
-  flag=False
-  while tries < 3:
-    m=int(input("Give me the number: "))
-    random_num=rd.randint(1,10)
-    if (m==random_num):
-       print("Correct guess")
-       break
-    else:
-       tries+=1
-       print("Please try again, You have got {0} tries.".format(3-tries) )
+  max_tries=3
 
+  while tries < max_tries:
+    try:
+      user_guess=int(input("Give me the number: "))
+    except ValueError:
+      print("!Please enter a valid number.")
+      continue
+    
+    if (user_guess==secret):
+      print("Congratulation. You guessed it!")
+      break
+    else:
+      tries+=1
+      remaining=max_tries-tries
+      if remaining > 0:
+        print("Please try again, You have {0} tries.".format(remaining))
+      else:
+        print(f" Game over! The correct number was {secret}.")
 
 
 def main():
-  guess()
+  while True:
+    guess()
+    again=input("Do you want to play again? (yes or no):").lower()
+    if again !="yes":
+      print("👋 Thanks for playing!")
+      break
+      
 
 
 if __name__=='__main__':
